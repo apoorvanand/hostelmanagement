19fec3619a4b276efa595bf4408607c5bfe4eff6
Matt Coshal <matthew.coshal@yale.edu>
2017-11-07 10:37:20 -0500

    Start to add factory

--- spec/factories/clips.rb
+++ spec/factories/clips.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 FactoryGirl.define do
   factory :clip do
-    association :draw, factory: :draw_with_members
+    association :draw #, factory: :draw_with_members, groups_count: 2
+
+    factory :clip_with_groups do
+      after(:build) do |c|
+        c.groups << create(:finalizing_group, draw: c.draw)
+        c.groups << create(:locked_group, draw: c.draw)
+      end
+    end
   end
 end
