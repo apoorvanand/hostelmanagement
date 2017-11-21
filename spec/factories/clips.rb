0f5c0fad98963dee741be9ece5228472356ce8ba
Matt Coshal <matthew.coshal@yale.edu>
2017-11-21 13:55:56 -0500

    Fix clip factory

--- spec/factories/clips.rb
+++ spec/factories/clips.rb
@@ -6,8 +6,12 @@ FactoryGirl.define do
       groups_count 2
     end
 
-    draw { create(:draw_with_groups, groups_count: groups_count) }
-    groups { draw.groups }
+    draw
+
+    after(:build) do |clip, e|
+      clip.groups = create_list(:group_from_draw, e.groups_count,
+                                draw: clip.draw)
+    end
 
     factory :clip_with_lottery_numbers do
       after(:build) do |clip|
