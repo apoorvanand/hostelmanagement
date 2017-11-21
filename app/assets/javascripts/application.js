0f09a9a8b8d8688d64dd597f5adbb6adc35a0a22
Sydney Young <sydney.young@yale.edu>
2017-11-21 16:25:08 -0500

    Basic enabling of bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require jquery
 //= require jquery-ui/effect
 //= require jquery_ujs
-//= require foundation
 
 //= require_tree .
 
