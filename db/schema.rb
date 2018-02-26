01f8d7daafd753f7b08e0707a304ae3df77ccbb1
Ariel <ariel.murphy@yale.edu>
2018-02-26 14:31:32 -0500

    reviewed changes on gitlab and saw some simple problems

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180216182524) do
-=======
-ActiveRecord::Schema.define(version: 20171208202737) do
->>>>>>> 8268183... fixing like controller
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
