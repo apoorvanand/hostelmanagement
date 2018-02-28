2530b2c2efa43187fc531b83c8c06b597a6e8490
Ariel <ariel.murphy@yale.edu>
2018-02-28 12:34:54 -0500

    fixed loose errors caused by mismatched if/ends

--- spec/features/draws/draw_suite_selection_spec.rb
+++ spec/features/draws/draw_suite_selection_spec.rb
@@ -89,7 +89,7 @@ RSpec.feature 'Draw suite selection' do
     before { log_in clip.groups.first.leader }
 
     it 'can view draw page' do
-      allow(groups).to receive(:locked).and_return(true)
+      allow(group).to receive(:status).and_return("locked")
       visit draw_path(draw)
       expect(page).to have_content(draw.name)
     end
