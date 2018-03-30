fb6143a863ba563522411ce17cdbef6fd5019b2f
Bryan Owens <bryan.owens@yale.edu>
2018-03-30 16:28:15 -0400

    Using binding.pry to debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
       @users['student'] = @users['student'].group_by(&:class_year)
     end
     @users.default = []
+    binding.pry
   end
 
   def show; end
