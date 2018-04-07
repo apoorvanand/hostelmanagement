36d6c9252b950747a7a5296a3f2cde2ba62135bf
Bryan Owens <bryan.owens@yale.edu>
2018-04-07 16:24:36 -0400

    Fix style for rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,10 +55,10 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :intent, presence: true
-  # rubocop:disable AlignHash
+  # rubocop:disable TrailingWhitespace
   validates :class_year, presence: true, 
             if: ->() { role == 'student' || role == 'rep' }
-  # rubocop:enable AlignHash
+  # rubocop:enable TrailingWhitespace
   validate :room_in_suite, if: ->() { group.present? && group.suite.present? }
 
   enum role: %w(student admin rep superuser)
