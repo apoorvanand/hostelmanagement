51f8e03e6c0229bb3738666ab7977a427e20c356
Bryan Owens <bryan.owens@yale.edu>
2018-04-07 16:37:52 -0400

    Fix style for rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,8 +55,10 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :intent, presence: true
-  validates :class_year, presence: true, 
-            if: ->() { role == 'student' || role == 'rep' }
+  validates :class_year, presence: true, if: 
+            ->(){ 
+                  role == 'student' || role == 'rep'
+                }
   validate :room_in_suite, if: ->() { group.present? && group.suite.present? }
 
   enum role: %w(student admin rep superuser)
